66f96a9530bdbf0011af8f08a44ef336ac7b685d
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-08-26 22:24:21 -0600

    Add RockPaperScissors

--- js/main.js
+++ js/main.js
@@ -1,14 +1,72 @@
-var ro = document.getElementById("ro");
-var sham = document.getElementById("sham");
-var bo = document.getElementById("bo");
+var winsnum = parseInt(document.getElementById("wins").innerHTML);
+var lossesnum = parseInt(document.getElementById("losses").innerHTML);
+var tiesnum = parseInt(document.getElementById("ties").innerHTML);
 
-ro.addEventListener('click', function() {
-  var firstname = document.getElementById('first_name');
-  var lastname = document.getElementById('last_name');
-  var list = document.getElementById('people'); 
-  var li = document.createElement('li');
-  li.innerHTML = lastname.value + ', ' + firstname.value;
-  list.appendChild(li);
-  firstname.value = "";
-  lastname.value = "";
+document.getElementById("Rock").addEventListener('click', function() {
+  var userAns = this.id
+  var compAnsNum = Math.floor((Math.random() * 3) + 1);
+  var lastGameMessage = document.getElementById("lastgame")
+  switch(compAnsNum) {
+    case 1:
+      lastGameMessage.innerText = "Computer chose Rock and you tied.";
+      tiesnum+=1;
+      document.getElementById("ties").innerHTML = tiesnum;
+      break;
+    case 2:
+      lastGameMessage.innerText = "Computer chose Paper and you lost.";
+      lossesnum+=1;
+      document.getElementById("losses").innerHTML = lossesnum;
+      break;
+    case 3:
+      lastGameMessage.innerText = "Computer chose Scissors and you won!";
+      winsnum+=1;
+      document.getElementById("wins").innerHTML = winsnum;
+      break;
+  }
+});
+
+document.getElementById("Paper").addEventListener('click', function() {
+  var userAns = this.id
+  var compAnsNum = Math.floor((Math.random() * 3) + 1);
+  var lastGameMessage = document.getElementById("lastgame")
+  switch(compAnsNum) {
+    case 1:
+      lastGameMessage.innerText = "Computer chose Rock and you won!";
+      winsnum+=1;
+      document.getElementById("wins").innerHTML = winsnum;
+      break;
+    case 2:
+      lastGameMessage.innerText = "Computer chose Paper and you tied.";
+      tiesnum+=1;
+      document.getElementById("ties").innerHTML = tiesnum;
+      break;
+    case 3:
+      lastGameMessage.innerText = "Computer chose Scissors and you lost.";
+      lossesnum+=1;
+      document.getElementById("losses").innerHTML = lossesnum;
+      break;
+  }
+});
+
+document.getElementById("Scissors").addEventListener('click', function() {
+  var userAns = this.id
+  var compAnsNum = Math.floor((Math.random() * 3) + 1);
+  var lastGameMessage = document.getElementById("lastgame")
+  switch(compAnsNum) {
+    case 1:
+      lastGameMessage.innerText = "Computer chose Rock and you lost.";
+      lossesnum+=1;
+      document.getElementById("losses").innerHTML = lossesnum;
+      break;
+    case 2:
+      lastGameMessage.innerText = "Computer chose Paper and you won!";
+      winsnum+=1;
+      document.getElementById("wins").innerHTML = winsnum;
+      break;
+    case 3:
+      lastGameMessage.innerText = "Computer chose Scissors and you tied.";
+      tiesnum+=1;
+      document.getElementById("ties").innerHTML = tiesnum;
+      break;
+  }
 });
